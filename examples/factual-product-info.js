4e7c4378d6eb90e579f4c84671c25759dd7d8a34
Zac McCormick <zac.mccormick@gmail.com>
2015-12-08 16:00:28 -0500

    Fix factual example

--- examples/factual-product-info.js
+++ examples/factual-product-info.js
@@ -3,18 +3,8 @@ var credentials = {
 };
 
 ON('change', 'barcode', function(event) {
-  GETAMAZONPRODUCTINFO(credentials, $barcode, function(err, result) {
-    var item = result.ItemLookupResponse.Items[0].Item;
-    var attrs = item[0].ItemAttributes[0];
-
-    var parts = [];
-
-    if (attrs.Title) parts.push(attrs.Title);
-    if (attrs.ListPrice) parts.push(attrs.ListPrice[0].FormattedPrice);
-    if (attrs.Model) parts.push(attrs.Model);
-    if (attrs.Feature) parts.push(attrs.Feature.join('\n\n'));
-
-    SETVALUE('product_info', parts.join("\n\n"));
+  GETFACTUALPRODUCTINFO(credentials, $barcode, function(err, result) {
+    SETVALUE('product_info', result);
   });
 });
 
